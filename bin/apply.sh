a82847bc4835d01be1b7264db4cc774cc428ed32
Matt Baker <m@wheres.co.uk>
2024-04-06 21:42:01 +0100

    Update apply.sh

--- bin/apply.sh
+++ bin/apply.sh
@@ -15,14 +15,16 @@ fi
   && ansible-galaxy role install -p "${VENV_DIR}/roles" -r requirements.yml
 )
 
+[ -z "$CI" ] && ANSIBLE_EXTRA_ARGS=("--ask-become-pass")
+
 # ANSIBLE_COW_SELECTION=none \
 ANSIBLE_CALLBACK_WHITELIST=unixy \
 ANSIBLE_STDOUT_CALLBACK=unixy \
-ANSIBLE_COLLECTIONS_PATHS="$VENV_DIR"/collections \
+ANSIBLE_COLLECTIONS_PATH="$VENV_DIR"/collections \
 ANSIBLE_ROLES_PATH="$VENV_DIR"/roles:"$base"/roles \
 ansible-playbook \
   -e @personal.yml \
   -i inventories/local \
-  --ask-become-pass \
+  "${ANSIBLE_EXTRA_ARGS[@]}" \
   "${ANSIBLE_PLAYBOOK:=playbooks/linux-desktop.yml}" \
   "$@"
