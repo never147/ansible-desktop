d6b8e633f475badee39ad03faba67cd79cef29c2
Matt Baker <m@wheres.co.uk>
2024-05-17 22:55:09 +0100

    Ubuntu 24.04 updates. Move to poetry

--- bin/apply.sh
+++ bin/apply.sh
@@ -2,17 +2,18 @@
 
 base=$(dirname "$0")/..
 cd "$base" || exit 1
-VENV_DIR=${VENV_DIR:="$HOME"/Apps/virtualenvs/home_dir}
 
-if [ ! -f "$VENV_DIR"/bin/activate ] ;then
+if [ ! -f ~/.local/bin/poetry ] ;then
     ./bin/bootstrap.sh
 fi
-. "$VENV_DIR"/bin/activate
-
+poetry install
+VENV_DIR=$(poetry env info -p)
 (
   cd "$base"/collections \
-  && ansible-galaxy collection install -p "${VENV_DIR}/collections" -r requirements.yml \
-  && ansible-galaxy role install -p "${VENV_DIR}/roles" -r requirements.yml
+  && poetry run ansible-galaxy collection install \
+      -p "${VENV_DIR}/collections" -r requirements.yml \
+  && poetry run ansible-galaxy role install \
+      -p "${VENV_DIR}/roles" -r requirements.yml
 )
 
 [ -z "$CI" ] && ANSIBLE_EXTRA_ARGS=("--ask-become-pass")
@@ -22,7 +23,7 @@ ANSIBLE_CALLBACK_WHITELIST=unixy \
 ANSIBLE_STDOUT_CALLBACK=unixy \
 ANSIBLE_COLLECTIONS_PATH="$VENV_DIR"/collections \
 ANSIBLE_ROLES_PATH="$VENV_DIR"/roles:"$base"/roles \
-ansible-playbook \
+poetry run ansible-playbook \
   -e @personal.yml \
   -i inventories/local \
   "${ANSIBLE_EXTRA_ARGS[@]}" \
--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,19 +1,14 @@
 #!/usr/bin/env bash
 set -x
-#BASE=$(dirname "$0")/..
 
-#sudo apt-get install -y --install-recommends python3-dev
+sudo apt-get install -y \
+  --install-recommends \
+  python3-full python3-dev \
+  pipx libffi-dev
 
 mkdir -p ~/.local/bin
 export PATH=$PATH:~/.local/bin
 
-if [ -f ~/.pip/pip.conf ] ;then
-    mv ~/.pip/pip.conf ~/.pip/pip.conv.orig
-fi
+pipx install poetry
 
-export WORKON_HOME=$HOME/Apps/virtualenvs
-mkdir -p "$WORKON_HOME"
-python3 -m venv "$WORKON_HOME"/home_dir
-. "$WORKON_HOME"/home_dir/bin/activate
-pip install --upgrade pip
-pip install -r requirements.txt
+poetry install
