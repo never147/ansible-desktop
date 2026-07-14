ab6f2bbe14d325f1043e2e86fd295b5c9692ac78
Matt Baker <m@wheres.co.uk>
2026-07-14 16:22:35 +0100

    Update apt first before installing packages

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 
-sudo apt-get install -y \
+sudo apt-get update && \
+  apt-get install -y \
   --install-recommends \
   python3-full python3-dev \
   pipx libffi-dev
